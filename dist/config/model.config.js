da70bbc8c2ca5ac3b58b106c0fd70d6779223b80
harsh-tank <65907699+harsh-tank@users.noreply.github.com>
2022-05-03 17:14:55 +0530

    deleted dist

--- dist/config/model.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sequelize = exports.Sequelize = void 0;
-var sequelize_1 = require("sequelize");
-Object.defineProperty(exports, "Sequelize", { enumerable: true, get: function () { return sequelize_1.Sequelize; } });
-var env = process.env.NODE_ENV || "development";
-var config = require("../config/config")[env];
-var sequelize = new sequelize_1.Sequelize(config.database, config.username, config.password, config);
-exports.sequelize = sequelize;
